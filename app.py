bf9ea8e4dd9dc0298405384032a6e529cfa69d29
Kristian Beech <48364159+kbeech98@users.noreply.github.com>
2024-01-30 14:07:17 -0800

    Kristian dev (#5)
    * Functioning csv uploader using static filepaths
    
    ---------
    
    Co-authored-by: Postle01 <jonnyphockey3@gmail.com>

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+import os
+import csv
+import app
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
-    text = ""
+def index():
+    data = []
     if request.method == 'POST':
-        text = request.form.get('avatar')
-    return render_template('index.html', text=text)
+        if request.files:
+            uploaded_file = request.files['filename'] # This line uses the same variable and worked fine
+            filepath = os.path.join(app.config['FILE_UPLOADS'], uploaded_file.filename)
+            uploaded_file.save(filepath)
+            with open(filepath) as file:
+                csv_file = csv.reader(file)
+                for row in csv_file:
+                    data.append(row)
+    return render_template('index.html', data=data)
 
+app.config['FILE_UPLOADS'] = "c:\\Users\\Krist\\Documents\\Personal\\UBCO FALL 2023\\ENGR 499\\Sim Data"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
