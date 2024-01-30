efb890f875e77c203405a70bf8a1f89a11ed9178
Postle01 <jonnyphockey3@gmail.com>
2024-01-30 12:55:10 -0800

    Updated drop box py and html code

--- app.py
+++ app.py
@@ -5,9 +5,21 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     text = ""
+    data = []
     if request.method == 'POST':
+        if request.files:
+            uploaded_file = request.files['file']
+            uploaded_file.save(os.path.join(app.config['FileUp'], uploaded_file.files))
+            f = request.form['filename']
+            with open(f) as files:
+                csv_file = csv.reader(file)
+                for row in csv_file:
+                    data.append(row)
+                return redirect(request.url)
         text = request.form.get('avatar')
-    return render_template('index.html', text=text)
+    return render_template('index.html', text=filename)
+    return render_templata('index.html', data=data)
 
+app.config['FILE_UPLOADS'] = "C:\Users\Jonathan\Downloads"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
