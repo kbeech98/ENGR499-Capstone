69b5df50f48e0010a72d56efadb746b85419b0df
Kristian Beech <48364159+kbeech98@users.noreply.github.com>
2024-01-30 13:52:21 -0800

    Functioning csv uploader using static filepaths

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+import os
+import csv
+import app
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
-    text = ""
+def index():
     data = []
     if request.method == 'POST':
         if request.files:
-            uploaded_file = request.files['file']
-            uploaded_file.save(os.path.join(app.config['FileUp'], uploaded_file.files))
-            f = request.form['filename']
-            with open(f) as files:
+            uploaded_file = request.files['filename'] # This line uses the same variable and worked fine
+            filepath = os.path.join(app.config['FILE_UPLOADS'], uploaded_file.filename)
+            uploaded_file.save(filepath)
+            with open(filepath) as file:
                 csv_file = csv.reader(file)
                 for row in csv_file:
                     data.append(row)
-                return redirect(request.url)
-        text = request.form.get('avatar')
-    return render_template('index.html', text=filename)
+    return render_template('index.html', data=data)
 
-app.config['FILE_UPLOADS'] = "C:\Users\Jonathan\Downloads"
+app.config['FILE_UPLOADS'] = "c:\\Users\\Krist\\Documents\\Personal\\UBCO FALL 2023\\ENGR 499\\Sim Data"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
