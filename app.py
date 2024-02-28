7ca87ba0d00dd62c1859baa81549db03dd33ab45
kbeech98 <kristianb@live.ca>
2024-02-28 12:58:52 -0800

    ALmost functional results

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, flash, request, redirect, url_for, sen
 from werkzeug.utils import secure_filename
 import os, csv, app
 from os.path import join, dirname, realpath
+import pickle
+import pandas as pd
+import numpy as np
 
 UPLOAD_FOLDER = dirname(realpath(__file__))
 
@@ -32,13 +35,56 @@ def upload_file():
         for file in find_csv_filenames(UPLOAD_FOLDER):
             remove(file)
     return redirect(url_for('index'))
+
+@app.route('/results', methods=['POST'])
+def get_results():
+    if "predict" in request.form:
+        process_prediction()
+
+    return render_template('results.html')
         
 def remove(file_id):
     return os.remove(os.path.join(UPLOAD_FOLDER, file_id))
 
-# def get_data():
-#     data = []
-#     if 
+def get_dataFrame(csv_filename):
+    df = pd.read_csv(os.path.join(app.config['UPLOAD_PATH'], csv_filename))
+    return df
+
+def process_prediction():
+    csv_file = find_csv_filenames(UPLOAD_FOLDER)
+    if csv_file:
+        df = get_dataFrame(csv_file[0])
+
+        # Calculate velocity magnitude
+        vel_x = np.array(df['velocity_X'])
+        vel_y = np.array(df['velocity_Y'])
+        vel_z = np.array(df['velocity_Z'])
+        vel_mag = np.sqrt(np.square(vel_x) + np.square(vel_y) + np.square(vel_z))
+        df['vel_mag'] = vel_mag
+        vel_matrix = np.zeros((len(vel_x),3))
+        vel_matrix[:,0] = vel_x
+        vel_matrix[:,1] = vel_y
+        vel_matrix[:,2] = vel_z
+
+        # Calculate gforce magnitude
+        g_x = np.array(df['gforce_X'])
+        g_y = np.array(df['gforce_Y'])
+        g_z = np.array(df['gforce_Z'])
+        g_mag = np.sqrt(np.square(g_x) + np.square(g_y) + np.square(g_z))
+        df['g_mag'] = g_mag
+        gforce_matrix = np.zeros((len(vel_x),3))
+        gforce_matrix[:,0] = g_x
+        gforce_matrix[:,1] = g_y
+        gforce_matrix[:,2] = g_z
+
+        # Project acceleration vector onto velocity vector
+        long_g = np.zeros(len(g_x))
+        for i in range(len(g_x)):
+            long_g[i] = np.dot(gforce_matrix[i,:],vel_matrix[i,:])/vel_mag[i]
+            
+
+    return 
+
 
 # @app.route('/', methods=['GET', 'POST'])
 # def index():
